51ab74370b95702d9e84a476d807137a6cb81d89
aurelie <aurelie.conan@lepetitvapoteur.com>
2022-07-28 17:45:22 +0200

    auth form ok

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.signUpForm = this.formBuilder.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.pattern(/[\da-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit(){
+    const email = this.signUpForm.get('email').value;
+    const password = this.signUpForm.get('password').value;
+    this.authService.createNewUser(email, password).then(
+      () => {
+        this.router.navigate(['/books']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import  firebase  from  'firebase/compat/app' ;
+import  'firebase/compat/auth' ;
+import  'firebase/compat/firestore' ;
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,38 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  createNewUser(email: string, password: string){
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    )
+  }
+
+  signInUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signOut(){
+    firebase.auth().signOut();
+  }
 }
